d94cdb64ef869bf869a6519399184b29a258f6ee
adleyaxel <adleymarchena@gmail.com>
2022-06-10 04:13:43 -0600

    Variables

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Variables");
+
+// int
+int age = 21;
+Console.WriteLine(age);
+
+int roomNumber;
+roomNumber = 4;
+Console.WriteLine(roomNumber);
+
+// double
+double pi = 3.14D;
+Console.WriteLine(pi);
+
+// char
+char firstLetter = 'A';
+Console.WriteLine(firstLetter);
+
+// bool
+bool acceptep = false;
+Console.WriteLine(acceptep);
+
+bool complete = true;
+Console.WriteLine(complete);
+
+// string
+string lastName = "Marchena";
+Console.WriteLine(lastName);
+
+// Programa de lectura y esritura I/O
+string fullName;
+Console.WriteLine("Ingrese su nombre Completo.");
+fullName = Console.ReadLine();
+
+Console.WriteLine("Su nombre es: ");
+Console.WriteLine(fullName);
+
+// ----
+Console.WriteLine("\n¿En qué país vide usted? ");
+string country = Console.ReadLine();
+
+Console.WriteLine("Su país es: ");
+Console.WriteLine(country);
